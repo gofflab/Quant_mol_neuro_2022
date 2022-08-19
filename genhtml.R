b5bb5ee3e8adf5cdc8ff37d4fcec9d98e7a9ce44
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-08-19 17:13:30 -0400

    Setup pandoc
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- genhtml.R
+++ genhtml.R
@@ -1,12 +1,12 @@
-files <- list.files(".", pattern="*.html$", recursive = TRUE)
+files <- list.files(".", pattern = "*.html$", recursive = TRUE)
 links <- paste(
-   lapply(files, function (x) paste('<a href="/', x, '">', x, '</a>', sep="")),
-   collapse="\n<br>\n"
+    lapply(files, function(x) paste('<a href="/', x, '">', x, "</a>", sep = "")),
+    collapse = "\n<br>\n"
 )
 html <- paste(
-   "<html><head><title>Quant</title></head><body>",
-   links,
-   "</body></html>",
-   sep=""
+    "<html><head><title>Quant</title></head><body>",
+    links,
+    "</body></html>",
+    sep = ""
 )
 writeLines(html, "index.html")
