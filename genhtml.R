d86d9d86c3743aac829c9fae571c2257e75a0fe0
chaichontat <34997334+chaichontat@users.noreply.github.com>
2022-08-20 15:24:49 +0000

    Deploying to gh-pages from @ gofflab/Quant_mol_neuro_2022@6a4d5469b13327dda36599efcfee2f79ee34bab6 🚀

--- genhtml.R
+++ genhtml.R
@@ -1,12 +1,12 @@
-files <- Sys.glob("**/*.html")
+files <- list.files(".", pattern = "*.html$", recursive = TRUE)
 links <- paste(
-   lapply(files, function (x) paste('<a href="/', x, '">', x, '</a>', sep="")),
-   collapse="\n<br>\n"
+    lapply(files, function(x) paste('<a href="/Quant_mol_neuro_2022/', x, '">', x, "</a>", sep = "")),
+    collapse = "\n<br>\n"
 )
 html <- paste(
-   "<html><head><title>Quant</title></head><body>",
-   links,
-   "</body></html>",
-   sep=""
+    "<html><head><title>Quant_mol_neuro_2022</title></head><body><h1>Quantitative Molecular Neuroscience 2022</h1><h2>Course Materials></h2>",
+    links,
+    "</body></html>",
+    sep = ""
 )
 writeLines(html, "index.html")
