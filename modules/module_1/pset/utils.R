0d7c3db88095f0493d9b1e4e86e677169752034f
acspiegel <alina.spiegel@gmail.com>
2022-08-31 03:53:03 +0000

    Deploying to gh-pages from @ gofflab/Quant_mol_neuro_2022@908bc27e6eadfdf8e8b7014fa0992d1710a727c2 🚀

---
+++ modules/module_1/pset/utils.R
@@ -0,0 +1,31 @@
+###---------------------------------------------
+# ME.440.825 Quantitative Neurogenomics
+# Problem Set 1
+# Dependencies and supplementary functions
+# Author: Alina Spiegel
+# Date: 8/30/2021
+###---------------------------------------------
+
+# load dependencies
+library(ggplot2)
+
+# function to calculate the statistical mode
+# arguments: 
+#   x         a vector or matrix 
+getMode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+# function to create side-by-side box plots of gene expression for each
+# cell class
+# arguments:
+#   df        a dataframe containing the column "cellClass" and a column 
+#             corresponding to the argument geneName with values 
+#             log2(expression + 1) for that gene
+#   geneName  a string specifying the gene of interest. Must also be a column in
+#             the dataframe df
+makeBP <- function(df, geneName) {
+  print(ggplot(df, aes_string("cellClass", geneName)) + geom_boxplot() +
+    ylab(paste(geneName, 'log2(counts+1)')))
+}
